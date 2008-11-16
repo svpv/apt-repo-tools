f83952ccc2139aa1fdb28748bb67f0ce08302f72
Alexey Tourbin <at@altlinux.ru>
2008-11-17 02:30:02 +0300

    tools/pkglist-query.cc: new program, obsoletes countpkglist
    Sample usage:
    $ bzcat .../pkglist.classic.bz2 | ./tools/pkglist-query '%{NAME} - %{SUMMARY}\n' - | head -10
    3proxy - Proxy server
    4th - Basic framework for creating application specific scripting languages
    7colors - Little addicting game, take over the gaming area with your color
    AutoScan - AutoScan - a utility for network exploration
    C - C - a pseudo interpreter of the C programming language
    ConsoleKit - System daemon for tracking users, sessions and seats
    ConsoleKit-x11 - X11-requiring add-ons for ConsoleKit
    Cultivation - Cultivation is a game about the interactions within a gardening community
    FSViewer.app - FSViewer is a NeXT FileViewer lookalike for Window Maker
    FlightGear - FlightGear - best opensource fly simulation engine
    $

---
+++ pkglist-query.cc
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <rpm/rpmlib.h>
+#include "config.h"
+
+int main(int argc, char *argv[])
+{
+    const char *progname = argv[0];
+    if (argc < 3) {
+	fprintf(stderr, "Usage: %s <format> <pkglist>...", progname);
+	return 2;
+    }
+    const char *format = argv[1];
+    int rc = 0;
+    const char *pkglist;
+    int ix = 2;
+    while ((pkglist = argv[ix++]) != NULL) {
+	FD_t Fd = Fopen(pkglist, "r");
+	if (Ferror(Fd)) {
+	    fprintf(stderr, "%s: %s: %s\n", progname, pkglist, Fstrerror(Fd));
+	    rc = 1;
+	    continue;
+	}
+	Header h;
+	while ((h = headerRead(Fd, HEADER_MAGIC_YES)) != NULL) {
+	    const char *err = "unknown error";
+#ifdef RPM_HAVE_HEADERFORMAT
+	    char *str = headerFormat(h, format, &err);
+#else
+	    char *str = headerSprintf(h, format, rpmTagTable, rpmHeaderFormats, &err);
+#endif
+	    if (str == NULL) {
+		rc = 1;
+		fprintf(stderr, "%s: %s: %s\n", progname, pkglist, err);
+	    }
+	    else {
+		fputs(str, stdout);
+		free(str);
+	    }
+	    headerFree(h);
+	}
+	Fclose(Fd);
+    }
+    return rc;
+}
+
+// ex:set ts=8 sts=4 sw=4 noet:
